4467b0d4972ea53114d57d973c5738ffb6846e54
oliviergimenez <olivier.gimenez@cefe.cnrs.fr>
2021-08-06 13:54:11 +0300

    Package unrecognized.

--- r-addons.R
+++ r-addons.R
@@ -20,7 +20,7 @@ local({
 #install.packages("lubridate")
 #install.packages("stringi")
 #install.packages("kableExtra")
-#install.packages("sf")
+install.packages("unmarked")
 
 ## ____________________________________________________
 
