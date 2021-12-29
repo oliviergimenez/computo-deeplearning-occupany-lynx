b7b09d380eb3a5380ea20eae13b1f5c98e4e12c2
oliviergimenez <olivier.gimenez@cefe.cnrs.fr>
2021-12-29 13:16:58 +0200

    cvms not on mini-conda?

--- r-addons.R
+++ r-addons.R
@@ -2,29 +2,19 @@
 ## R packages needed not available via mini-conda
 ## ====================================================
 
-## ____________________________________________________
 ## Setting-up the source repository
 local({
   r <- getOption("repos")
   r["CRAN"] <- "https://cloud.r-project.org"
   options(repos = r)
 })
-## ____________________________________________________
 
-## ____________________________________________________
 ## Additional R packages needed by the user (CRAN)
-## example: remote
-## install.packages("anRpackage")
 install.packages("exifr")
+install.packages("cvms")
 remotes::install_github("rbchan/unmarked")
-## ____________________________________________________
-
-## ____________________________________________________
-## Additional R packages needed by the user ()
-#devtools::install_github("rbchan/unmarked")
 reticulate::install_miniconda()
 reticulate::conda_create('r-reticulate')
 ##remotes::install_github('eagerai/fastai')
 install.packages("fastai")
 #fastai::install_fastai(gpu = FALSE, cuda_version = '10', overwrite = FALSE)
-## ____________________________________________________
