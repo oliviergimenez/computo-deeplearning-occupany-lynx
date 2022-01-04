52908df5fc13071b93b519cb9fc6589b80996257
oliviergimenez <olivier.gimenez@cefe.cnrs.fr>
2022-01-04 22:42:56 +0100

    Use bits of fastai yml https://github.com/EagerAI/fastai/blob/master/.github/workflows/mac_os.yml

--- r-addons.R
+++ r-addons.R
@@ -13,10 +13,10 @@ local({
 install.packages("exifr")
 install.packages("cvms")
 remotes::install_github("rbchan/unmarked")
-reticulate::install_miniconda()
-reticulate::conda_create('r-reticulate')
-#devtools::install_github('eagerai/fastai')
-install.packages("fastai")
-fastai::install_fastai(gpu = FALSE, 
-                       cuda_version = '10', 
-                       overwrite = FALSE)
+# reticulate::install_miniconda()
+# reticulate::conda_create('r-reticulate')
+# #devtools::install_github('eagerai/fastai')
+# install.packages("fastai")
+# fastai::install_fastai(gpu = FALSE, 
+#                        cuda_version = '10', 
+#                        overwrite = FALSE)
