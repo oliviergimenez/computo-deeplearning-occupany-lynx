6501712866eccfc35362cfbb99dfed98b6bfa992
oliviergimenez <olivier.gimenez@cefe.cnrs.fr>
2022-03-29 15:06:29 +0200

    fastai again...

--- r-addons.R
+++ r-addons.R
@@ -21,8 +21,8 @@ reticulate::install_miniconda()
 reticulate::conda_create('r-reticulate', python_version = '3.9')
 remotes::install_github('eagerai/fastai')
 #install.packages("fastai")
-# fastai::install_fastai()
+fastai::install_fastai()
 #remotes::install_version("reticulate", version = "1.21", repos = "http://cran.us.r-project.org")
-fastai::install_fastai(gpu = FALSE, cuda_version = '10', overwrite = FALSE)
+#fastai::install_fastai(gpu = FALSE, cuda_version = '10', overwrite = FALSE)
 
 
